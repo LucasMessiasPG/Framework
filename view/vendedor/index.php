1b9bfea6402039dc5806399d492cf2d0ddaf15b8
jonastdr <tortato.jonas@gmail.com>
2015-04-30 20:40:53 -0300

    sfjgfsjgsjgjf

--- view/vendedor/index.php
+++ view/vendedor/index.php
@@ -1,5 +1,5 @@
 <?php $this->render('header') ?>
-<h3>Concessionaria</h3>
+<h3>Concessionária</h3>
 <a href="http://localhost/concessionaria" class="btn btn-default">Veículos</a>
 <a href="<?php echo URL ?>index/listar" class="btn btn-default">Vendedores</a>
 <?php $this->render('footer') ?>
